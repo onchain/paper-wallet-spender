54aac52b7dd247b955ca1ed2f0c13c17bb20167f
John Doe <ian.purton+c9@gmail.com>
2018-07-20 07:31:57 +0000

    Use the index.

--- src/onchain/protocol/transaction.cr
+++ src/onchain/protocol/transaction.cr
@@ -17,7 +17,7 @@ module OnChain
             blake_hash = zcash_tx.signature_hash_for_zcash(
               i.to_u64, unspent.amount)
             hashes_to_sign << HashToSign.new(
-              blake_hash, unspents.pub_hex_keys[i], unspent.vout)
+              blake_hash, unspents.pub_hex_keys[i], i)
           end
           
           return UnsignedTransaction.new(zcash_tx.to_hex, 
