d41f9011359d977d2b6fc4349429a7cd2301db9d
John Doe <ian.purton+c9@gmail.com>
2018-07-27 09:53:41 +0000

    Added more tests.

--- spec/address_spec.cr
+++ spec/address_spec.cr
@@ -4,15 +4,36 @@ describe OnChain do
 
   it "should parse and create vaid addresses" do
   
-    addr = OnChain::Protocol::Address.new(OnChain::CoinType::Bitcoin,
+    OnChain::Protocol::Address.new(OnChain::CoinType::Bitcoin,
+      "17iESYBf7CQMxCxdabiMfjZRDniGDZkyX3").to_s.should eq(
       "17iESYBf7CQMxCxdabiMfjZRDniGDZkyX3")
     
-    addr.to_s.should eq("17iESYBf7CQMxCxdabiMfjZRDniGDZkyX3")
-    
-  
-    addr = OnChain::Protocol::Address.new(OnChain::CoinType::Bitcoin,
+    OnChain::Protocol::Address.new(OnChain::CoinType::Bitcoin,
+      "36owNcemLHrqW6XXFWyXeedQoErSBWTQFE").to_s.should eq(
       "36owNcemLHrqW6XXFWyXeedQoErSBWTQFE")
     
-    addr.to_s.should eq("36owNcemLHrqW6XXFWyXeedQoErSBWTQFE")
+    OnChain::Protocol::Address.new(OnChain::CoinType::Bitcoin,
+      "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH").to_s.should eq(
+      "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH")
+    
+    OnChain::Protocol::Address.new(OnChain::CoinType::Bitcoin,
+      "3LRW7jeCvQCRdPF8S3yUCfRAx4eqXFmdcr").to_s.should eq(
+      "3LRW7jeCvQCRdPF8S3yUCfRAx4eqXFmdcr")
+    
+    OnChain::Protocol::Address.new(OnChain::CoinType::Litecoin,
+      "MCZjFcwYJwwYqXAbd3bbnxaCVGs81cp43Z").to_s.should eq(
+      "MCZjFcwYJwwYqXAbd3bbnxaCVGs81cp43Z")
+    
+    OnChain::Protocol::Address.new(OnChain::CoinType::Litecoin,
+      "LUxXFcwXFPpRZdMv4aYu6bDwPdC2skQ5YW").to_s.should eq(
+      "LUxXFcwXFPpRZdMv4aYu6bDwPdC2skQ5YW")
+    
+    OnChain::Protocol::Address.new(OnChain::CoinType::Testnet3,
+      "mrCDrCybB6J1vRfbwM5hemdJz73FwDBC8r").to_s.should eq(
+      "mrCDrCybB6J1vRfbwM5hemdJz73FwDBC8r")
+    
+    OnChain::Protocol::Address.new(OnChain::CoinType::Testnet3,
+      "2NByiBUaEXrhmqAsg7BbLpcQSAQs1EDwt5w").to_s.should eq(
+      "2NByiBUaEXrhmqAsg7BbLpcQSAQs1EDwt5w")
   end
 end
